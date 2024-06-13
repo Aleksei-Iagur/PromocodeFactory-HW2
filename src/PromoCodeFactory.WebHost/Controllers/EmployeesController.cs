8178be8515b3afbb3a14cd3576a74e1f436ddf28
Sergey <chaositomail@gmail.com>
2024-06-13 19:03:06 +0500

    Added methods Create, Update, Delete

--- src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
+++ src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -69,6 +70,94 @@ namespace PromoCodeFactory.WebHost.Controllers
             };
 
             return employeeModel;
+        }
+
+        /// <summary>
+        /// Создание сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<EmployeeResponse>> CreateEmployeeAsync(String firstName, String lastName, String eMail)
+        {
+
+            var usr = new Employee();
+            usr.Id = Guid.NewGuid();
+            usr.FirstName = firstName;
+            usr.LastName = lastName;
+            usr.Email = eMail;
+            usr.AppliedPromocodesCount = new Random().Next(0, 10);
+
+            usr.Roles = new List<Role>()
+            {
+                new Role { Name = "User" }
+            };
+
+            var d = await _employeeRepository.CreateRecord(usr);
+
+            var employeeModel = new EmployeeResponse()
+            {
+                Id = d.Id,
+                Email = d.Email,
+                Roles = d.Roles.Select(x => new RoleItemResponse()
+                {
+                    Name = x.Name,
+                    Description = x.Description
+                }).ToList(),
+                FullName = d.FullName,
+                AppliedPromocodesCount = d.AppliedPromocodesCount
+            };
+
+            return employeeModel;
+        }
+
+        /// <summary>
+        /// Редактирование сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<ActionResult<EmployeeResponse>> UpdateEmployeeAsync(Guid id, String firstName, String lastName, String eMail)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            employee.FirstName = firstName;
+            employee.LastName = lastName;
+            employee.Email = eMail;
+
+            var d = await _employeeRepository.UpdateRecord(id, employee);
+
+            var employeeModel = new EmployeeResponse()
+            {
+                Id = d.Id,
+                Email = d.Email,
+                Roles = d.Roles.Select(x => new RoleItemResponse()
+                {
+                    Name = x.Name,
+                    Description = x.Description
+                }).ToList(),
+                FullName = d.FullName,
+                AppliedPromocodesCount = d.AppliedPromocodesCount
+            };
+
+            return employeeModel;
+        }
+
+        /// <summary>
+        /// Удаление сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task<ActionResult> DeleteEmployeeAsync(Guid id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            await _employeeRepository.DeleteRecord(id);
+            return Ok();
         }
     }
 }
