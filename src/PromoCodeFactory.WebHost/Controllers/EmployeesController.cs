6bd12a2b387995aa2eebb6e1587b905b0ec245fd
Alla-Martyshova <Alla.Martyshova@tehnomer.ru>
2024-06-17 01:15:35 +0300

    HW2: Add Create/Update/Delete to EmployeesController

--- src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
+++ src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PromoCodeFactory.Core.Abstractions.Repositories;
@@ -69,6 +70,55 @@ namespace PromoCodeFactory.WebHost.Controllers
             };
 
             return employeeModel;
+        }
+
+       
+        /// <summary>
+        /// Добавить сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployeeAsync(Employee employee)
+        {
+            var entity = await _employeeRepository.CreateAsync(employee);
+
+            return Ok(entity);
+        }
+
+        /// <summary>
+        /// Обновить данные сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpPatch]
+        public async Task<IActionResult> UpdateEmployeeAsync(Employee employee)
+        {
+            var model = await _employeeRepository.GetByIdAsync(employee.Id);
+
+            if (model == null)
+                return NotFound();
+
+            var entity = await _employeeRepository.UpdateAsync(employee);
+
+            return Ok(entity);
+        }
+
+
+
+        /// <summary>
+        /// Удалить сотрудника
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteEmployeeAsync(Guid id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            await _employeeRepository.DeleteAsync(id);
+
+            return Ok(id);
         }
     }
 }
